fa4d8fc9fc9b87f55bd147daea30ed62b595bb5d
akgooljar07 <79831705+akgooljar07@users.noreply.github.com>
2021-03-21 01:36:07 +0400

    Update mulTwolnt.py

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -4,27 +4,29 @@ def mul(x,y):
     return x*y
 
 
+def main():
+    try:
 
-try:
+        num1,num2=(input("Entrez 2 nombres :")).split( )
+        w=int(num1)
+        n=int(num2)
+        z=mul(w,n)
+        print("The mul : ", z)
 
-    num1,num2=(input("Entrez 2 nombres :")).split( )
-    w=int(num1)
-    n=int(num2)
-    z=mul(w,n)
-    print("The mul : ", z)
-
-except (ValueError):
-    print("please entrez 2 nombres svp :")
-    num1,num2=(input("Entrez 2 nombres :")).split( )
-    w=int(num1)
-    n=int(num2)
-    z=mul(w,n)
-    print("The mul : ", z)
-except (ValueError):
-    print("please entrez 2 nombres svp :")
-    num1,num2=(input("Entrez 2 nombres :")).split( )
-    w=int(num1)
-    n=int(num2)
-    z=mul(w,n)
-    print("The mul : ", z)
+    except (ValueError):
+        print("please entrez 2 nombres svp :")
+        num1,num2=(input("Entrez 2 nombres :")).split( )
+        w=int(num1)
+        n=int(num2)
+        z=mul(w,n)
+        print("The mul : ", z)
+    except (ValueError):
+        print("please entrez 2 nombres svp :")
+        num1,num2=(input("Entrez 2 nombres :")).split( )
+        w=int(num1)
+        n=int(num2)
+        z=mul(w,n)
+        print("The mul : ", z)
+        
+main()
 
