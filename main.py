9dcd4960e0f40bf77f8c9ddd2f2ea64f8775abfb
akgooljar07 <79831705+akgooljar07@users.noreply.github.com>
2021-03-21 01:39:40 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,29 @@ def add(x,y):
 def mul(x,y):
     return x*y
 
-def main():
 
-	try:
+try:
 
-	   num1,num2=(input("enter 2 number : ")).split( )
-	   w=int(num1)
-	   n=int(num2)
-	   print("Choose Your Operation!")
-	   print("1.Addition")
-	   print("2.Multiplication")
-	   choice = int(input("Choose Your Operation : "))
-	except (ValueError):
+   num1,num2=(input("enter 2 number : ")).split( )
+   w=int(num1)
+   n=int(num2)
+   print("Choose Your Operation!")
+   print("1.Addition")
+   print("2.Multiplication")
+   choice = int(input("Choose Your Operation : "))
+except (ValueError):
 
-	   print("Please input  2 numbers !")
-	   num1,num2=(input("enter 2 number : ")).split( )
-	   w=int(num1)
-	   n=int(num2)
-	   choice = int(input("Choose Your Operation :  "))
+   print("Please input  2 numbers !")
+   num1,num2=(input("enter 2 number : ")).split( )
+   w=int(num1)
+   n=int(num2)
+   choice = int(input("Choose Your Operation :  "))
 
 
-	if (choice=="1"):
-	  z=add(w,n)
-	  print("The sum : ",z)
+if (choice=="1"):
+  z=add(w,n)
+  print("The sum : ",z)
 
-	else:
-	    m=mul(w,n)
-	    print("The Multiplication : ",m)
-main()
+else:
+    m=mul(w,n)
+    print("The Multiplication : ",m)
