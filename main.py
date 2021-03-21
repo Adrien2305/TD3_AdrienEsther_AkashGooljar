062df123a689d76b4f3741547cbc3559ec42dec1
akgooljar07 <79831705+akgooljar07@users.noreply.github.com>
2021-03-21 11:27:42 +0400

    [dev]-Nouveau Modification

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ try:
    num1,num2=(input("enter 2 number : ")).split( )
    w=int(num1)
    n=int(num2)
-   print("Choose Your Operation!")
+   print("Choose 1 / 2 : ")
    print("1.Addition")
    print("2.Multiplication")
    choice = int(input("Choose Your Operation : "))
@@ -26,10 +26,10 @@ except (ValueError):
    choice = int(input("Choose Your Operation :  "))
 
 
-if (choice=="1"):
-  z=add(w,n)
-  print("The sum : ",z)
+if (choice== 1 ):
+  	z=add(w,n)
+  	print("The sum : ",z)
 
 else:
-    m=mul(w,n)
-    print("The Multiplication : ",m)
+    	m=mul(w,n)
+    	print("The Multiplication : ",m)
